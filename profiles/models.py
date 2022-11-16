c56a24f3a0386d3f42842bfd5466b504f073e393
odiegofarias <diego.farias.fernandes@gmail.com>
2022-11-16 20:35:26 -0300

    Criado model 'relationship', para os relacionamentos entre partes. Criado signals que cria o relacionamento de amizade automaticamente com envio, quando o 'status' for alterado para 'accepted'.

--- profiles/models.py
+++ profiles/models.py
@@ -19,7 +19,7 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return f"{self.user.username}-{self.created}"
+        return f"{self.user.username}-{self.created.strftime('%d-%m-%Y')}"
 
     def save(self, *args, **kwargs):
         ex = False
@@ -35,3 +35,19 @@ class Profile(models.Model):
         self.slug = to_slug
 
         super().save(*args, **kwargs)
+
+
+STATUS_CHOICES = (
+    ('send', 'send'),
+    ('accepted', 'accepted')
+)
+
+class Relationship(models.Model):
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='receiver')
+    status = models.CharField(max_length=8, choices=STATUS_CHOICES)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"{self.sender}-{self.receiver}-{self.status}"
--- profiles/signals.py
+++ profiles/signals.py
@@ -1,10 +1,34 @@
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, Relationship
 
 
+"""
+    Aqui temos a criação automática de Perfil com base no USER do Django.
+    No 'post_save', criamos o perfil com base na instancia do usuário que se encontra no USER
+"""
 @receiver(post_save, sender=User)
 def post_save_create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+
+"""
+    Criação do relacionamento entre os usuários com a amizade.
+    Um perfil envia o pedido de amizade para outro. Quando o status muda para 'accepted',
+    os perfis começam um relacionamento de amizade entre si.
+        print('sender: ', sender_)
+        print('receiver: ', receiver_)
+            Acima, temos o exemplo sender: Diego, receiver: testeuser2
+"""
+@receiver(post_save, sender=Relationship)
+def post_save_add_to_friend(sender, instance, created, **kwargs):
+    sender_ = instance.sender
+    receiver_ = instance.receiver
+
+    if instance.status == 'accepted':
+        sender_.friends.add(receiver_.user)
+        receiver_.friends.add(sender_.user)
+        sender_.save()
+        receiver_.save()
