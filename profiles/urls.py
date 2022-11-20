05a4b87c1ed001f00d60bb9a656cb5892b59fc90
odiegofarias <diego.farias.fernandes@gmail.com>
2022-11-20 16:08:35 -0300

    Criando Manager em Profiles e Relationship para os relacionamentos entre usuários

--- profiles/urls.py
+++ profiles/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'profiles'
 
 urlpatterns = [
-    path('myprofile/', views.my_profile_view, name='my-profile-view')
+    path('myprofile/', views.my_profile_view, name='my-profile-view'),
+    path('my-invites/', views.invites_received_view, name='my-invites-view'),
+    path('all-profiles/', views.profiles_list_view, name='all-profiles-view'),
+    path('to-invite/', views.invites_profiles_list_view, name='invites-profile-view'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, Relationship
 
 # Create your views here.
 def my_profile_view(request):
@@ -10,3 +10,33 @@ def my_profile_view(request):
     }
 
     return render(request, 'profiles/myprofile.html', context)
+
+def invites_received_view(request):
+    profile = Profile.objects.get(user=request.user)
+    qs = Relationship.objects.invatations_received(profile)
+
+    context = {
+        'qs': qs,
+    }
+
+    return render(request, 'profiles/my_invites.html', context)
+
+def profiles_list_view(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles(user)
+
+    context = {
+        'qs': qs,
+    }
+
+    return render(request, 'profiles/profile_list.html', context)
+
+def invites_profiles_list_view(request):
+    user = request.user
+    qs = Profile.objects.get_all_profiles_to_invite(user)
+
+    context = {
+        'qs': qs,
+    }
+
+    return render(request, 'profiles/to_invite_list.html', context)
