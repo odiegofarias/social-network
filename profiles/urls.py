dd47c294ed6d972d2ba18ac544c6fd0c653ce8ad
odiegofarias <diego.farias.fernandes@gmail.com>
2022-11-20 17:04:09 -0300

    Troca de FBV para CBV na profile list e criada lógica para verificar os relacionamentos. Aprensentando os relacionamentos com seus respectivos botões no template

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,6 +7,6 @@ app_name = 'profiles'
 urlpatterns = [
     path('myprofile/', views.my_profile_view, name='my-profile-view'),
     path('my-invites/', views.invites_received_view, name='my-invites-view'),
-    path('all-profiles/', views.profiles_list_view, name='all-profiles-view'),
+    path('all-profiles/', views.ProfileListView.as_view(), name='all-profiles-view'),
     path('to-invite/', views.invites_profiles_list_view, name='invites-profile-view'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Profile, Relationship
+from django.views.generic import ListView
+from django.contrib.auth.models import User
 
 # Create your views here.
 def my_profile_view(request):
@@ -21,16 +23,6 @@ def invites_received_view(request):
 
     return render(request, 'profiles/my_invites.html', context)
 
-def profiles_list_view(request):
-    user = request.user
-    qs = Profile.objects.get_all_profiles(user)
-
-    context = {
-        'qs': qs,
-    }
-
-    return render(request, 'profiles/profile_list.html', context)
-
 def invites_profiles_list_view(request):
     user = request.user
     qs = Profile.objects.get_all_profiles_to_invite(user)
@@ -40,3 +32,41 @@ def invites_profiles_list_view(request):
     }
 
     return render(request, 'profiles/to_invite_list.html', context)
+
+
+
+class ProfileListView(ListView):
+    model = Profile
+    template_name = 'profiles/profile_list.html'
+    # Padrão é object_list
+    context_object_name = 'qs'
+
+    def get_queryset(self):
+        qs = Profile.objects.get_all_profiles(self.request.user)
+        
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = User.objects.get(username__iexact=self.request.user)
+        profile = Profile.objects.get(user=user)
+        #Querysets
+        rel_r = Relationship.objects.filter(sender=profile)
+        rel_s = Relationship.objects.filter(receiver=profile)
+        rel_receiver = []
+        rel_sender = []
+
+        for item in rel_r:
+            rel_receiver.append(item.receiver.user)
+        for item in rel_s:
+            rel_receiver.append(item.sender.user)
+
+        context['rel_receiver'] = rel_receiver
+        context['rel_sender'] = rel_sender
+        context['is_empty'] = False
+
+        if len(self.get_queryset()) == 0:
+            context['is_empty'] = True
+
+        return context
+     
